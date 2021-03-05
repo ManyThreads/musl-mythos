88986a3bd33c6b5b97c8ec4502cc97093155cfe9
Philipp Gypser <gypsephi@b-tu.de>
2021-03-05 09:56:34 +0100

    cleanup thread list in revoke demand

--- src/thread/mythos_revoke_demand.c
+++ src/thread/mythos_revoke_demand.c
@@ -11,6 +11,11 @@ int mythos_revoke_demand(pthread_t pthread)
 {
   int res = mythos_revoke_demand_hook(pthread);
   if(res == 0){
+    __tl_lock();
+    pthread->next->prev = pthread->prev;
+    pthread->prev->next = pthread->next;
+    pthread->prev = pthread->next = pthread;
+		__tl_unlock();
     munmap(pthread->map_base, pthread->map_size);
   }
   return res;
